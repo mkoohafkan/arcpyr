06c7ae5b51be8e2b4a2f19ba97c567accfd4c0aa
mkoohafkan <michael.koohafkan@gmail.com>
2015-11-30 06:17:52 -0800

    Export some Spatial Analyst tools and a new function for raster calculations. TODO: export functions that differ from Python implementations, e.g. RasterCalculator.

--- R/initialize.r
+++ R/initialize.r
@@ -9,11 +9,14 @@
 #' @param quietly Logical: Suppress messages and warnings.
 #' @return No return value.
 #' @details The following arcpy calls are available:
+#' \subsection{Environment Settings}{
 #'   \itemize{
-#'     \item \strong{Environment Settings}
 #'     \item env.workspace
 #'     \item env.overwriteOutput
-#'     \item \strong{Listing}
+#'   }
+#' }
+#' \subsection{Listing}{
+#'   \itemize{
 #'     \item ListDatasets 
 #'     \item ListFeatureClasses 
 #'     \item ListFields 
@@ -23,14 +26,33 @@
 #'     \item ListTables 
 #'     \item ListVersions 
 #'     \item ListWorkspaces
-#'     \item \strong{Extraction}
+#'   }
+#' }
+#' \subsection{Extraction}{
+#'   \itemize{
 #'     \item Clip_analysis 
 #'     \item Select_analysis
 #'   }
+#' }
+#' \subsection{Management}{
+#'   \itemize{
+#'     \item AddJoin_management
+#'     \item RemoveJoin_management
+#'     \item JoinField_management
+#'   }
+#' }
+#' \subsection{Conversion}{
+#'   \itemize{
+#'     \item RasterToPolygon_conversion
+#'   }
+#' }
 #'
 #' @examples
 #' \dontrun{
 #' arcpy.initialize("C:/Python27/ArcGIS10.3/python.exe")
+#' env.workspace("C:/test.gdb")
+#' env.overwriteOutput(TRUE)
+#' Clip_analysis("feature1", "feature2")
 #' }
 #'
 #' @export
@@ -99,7 +121,96 @@ arcpy.initialize <- function(PYTHON_EXE, quietly = FALSE){
       "Select_analysis"
     )
     PythonInR::pyImport(extraction.funs, from = "arcpy")
-
+    # management
+    management.funs = c(
+      "Delete_management",
+      "CopyRows_management",
+      "AddJoin_management",
+      "RemoveJoin_management",
+      "JoinField_management"
+    )
+    PythonInR::pyImport(management.funs, from = "arcpy")
+    # conversion
+    conversion.funs = c(
+      "RasterToPolygon_conversion"
+    )
+    PythonInR::pyImport(conversion.funs, from = "arcpy")
   })
+  invisible(NULL)
+}
+
+#' Initialize Spatial Analyst Tools
+#'
+#' Initialize functions from the Spatial Analyst module.
+#'
+#' @return No return value.
+#' @details The following arcpy.sa calls are available:
+#' \subsection{Raster tools}{
+#'   \itemize{
+#'     \item RasterCalculator
+#'   }
+#' }
+#'
+#' @examples
+#' \dontrun{
+#' sa.initialize()
+#' RasterCalculator(expr, "outrast")
+#' }
+#'
+#' @export
+sa.initialize = function(){
+  with(parent.frame(), {
+    PythonInR::pyExec('arcpy.CheckOutExtension("Spatial")')
+    # RasterCalculator
+    RasterCalculator = function(expressions, inrasters = list(), 
+      outrasters = list()){
+      load_exprs = paste0(names(inrasters), ' = Raster("', inrasters, '")')
+      lapply(load_exprs, PythonInR::pyExec)
+      lapply(expressions, PythonInR::pyExec)
+      save_exprs = paste0(names(outrasters), '.save("', outrasters,'")')
+      lapply(save_exprs, PythonInR::pyExec)
+      invisible(NULL)
+    }
+    sa.funs = c(
+      "Raster",
+      "Con",
+      "Pick",
+      "SetNull",
+      "Abs",
+      "Exp",
+      "Exp10",
+      "Exp2",
+      "Float",
+      "Int",
+      "Ln",
+      "Log10",
+      "Log2",
+      "Mod",
+      "Power",
+      "RoundDown",
+      "RoundUp",
+      "Square",
+      "SquareRoot",
+      "ACos",
+      "ACosH",
+      "ASin",
+      "ASinH",
+      "ATan",
+      "ATan2",
+      "ATanH",
+      "Cos",
+      "CosH",
+      "Sin",
+      "SinH",
+      "Tan",
+      "TanH",
+      "Diff",
+      "InList",
+      "IsNull",
+      "Over",
+      "Test"
+    )    
+    PythonInR::pyImport(sa.funs, from = "arcpy.sa")
+  }) 
   invisible(NULL)
 }
