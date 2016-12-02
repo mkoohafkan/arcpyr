a6d7791a0f79dc42292e4e755b6d22429df349fb
mkoohafkan <michael.koohafkan@gmail.com>
2016-12-02 12:01:29 -0800

    major update
    Switched to toolbox-based importing and attaching. Removed dependency on `inspect` module. Moved Raster Calculator function to new package `arcpyrextra`.

--- R/buildenv.r
+++ R/buildenv.r
@@ -1,36 +1,28 @@
-#' Raster Calculation with arcpyr
+#' R Interface to ArcPy
 #'
-#' Raster Calculation with arcpy is handled via this helper function.
-#'
-#' @param expressions A list of raster calculation expressions to be 
-#'   executed in sequence.
-#' @param inrasters A named list of file paths to Rasters used in the 
-#'   calculation expressions, to be converted to ArcPy raster objects. 
-#' @param outrasters A named list of file paths to save Rasters 
-#'   resulting the calculation expressions.
-#' @return No return value; raster files listed in \code{outrasters} 
-#'   will be saved.
-#'
-RasterCalculator = function(expressions, inrasters = list(), outrasters = list()){
-  load_exprs = paste0(names(inrasters), ' = Raster("', inrasters, '")')
-  lapply(load_exprs, PythonInR::pyExec)
-  lapply(expressions, PythonInR::pyExec)
-  save_exprs = paste0(names(outrasters), '.save("', outrasters,'")')
-  lapply(save_exprs, PythonInR::pyExec)
-  invisible(NULL)
-}
+#' An R interface for the ArcGIS Python module \code{arcpy}. Relies on 
+#' the virtual Python environment provided by \code{PythonInR} to generate 
+#' function interfaces for \code{arcpy} tools and environment settings.
+#' \code{arcpy} toolboxes and extensions are accessed via an R environment
+#' object. See the vignette to get started.
+#' @name arcpyr-package
+#' @aliases arcpyr
+#' @docType package
+#' @seealso \code{\link{arcpy_env}}, \code{\link{connect_ArcPython}}
+NULL
+
 
 #' @importFrom utils head
 #' @importFrom utils tail
-envfun = function(value){
+envfun = function(value) {
   field = tail(unlist(
     strsplit(deparse(match.call()), split = "$", fixed = TRUE)), 1)
   field = head(unlist(strsplit(field, "(", fixed = TRUE)), 1)
-  if(!missing(value)){
-    if(is.logical(value)){
+  if (!missing(value)) {
+    if (is.logical(value)) {
       value = gsub("TRUE", "True", value)
       value = gsub("FALSE", "False", value)
-    } else if(is.character(value)){
+    } else if (is.character(value)) {
       value = paste0("'", value, "'")
     }
     PythonInR::pyExec(paste0('arcpy.env.', field, ' = ', value))
@@ -38,33 +30,60 @@ envfun = function(value){
   PythonInR::pyGet(paste0("arcpy.env.", field))
 }
 
-get_funs = function(module, predicate = "inspect.isfunction"){
-  PythonInR::pyExec("import inspect")
-  tv = gsub("\\.", "", paste0("objlist", predicate, module))
-  PythonInR::pyExec(paste0(tv, "= [i[0] for i in inspect.getmembers(", 
-    module, ",", predicate, ")]"))
-  if (PythonInR::pyGet(sprintf("len(%s)", tv)) < 1)
-    res = list()
-  else
-    res = PythonInR::pyGet(tv)
-  PythonInR::pyExec(paste("del", tv))
-  res
-}
-
-checkout_extension = function(ext){
-  PythonInR::pyExec(paste0('arcpy.CheckOutExtension("', ext, '")'))
+checkout_extension = function(ext) {
+  if (PythonInR::pyGet(sprintf('arcpy.CheckOutExtension("%s")', ext)) != "CheckedOut")
+    stop(sprintf("extension '%s' is not available.", ext), call. = FALSE)
   invisible(NULL)
 }
 
-import_module = function(module, function.list){
-PythonInR::pyExec(paste('import', module))
-  module.env = new.env(parent = emptyenv())
-  if(is.null(function.list))
-    function.list = get_funs(module)
-  for(fun in function.list)
-    assign(fun, PythonInR::pyFunction(paste(module, fun, sep = ".")), 
-      pos = module.env)
-  module.env
+import_toolbox = function(toolbox, tools) {
+  switch(toolbox,
+    "sa" = checkout_extension("Spatial"),
+    "ga" = checkout_extension("Geostats"),
+    "na" = checkout_extension("Network"),
+    "3d" = checkout_extension("3D"),
+    "ta" = checkout_extension("Tracking"),
+    "ba" = checkout_extension("Business"),
+    "interop" = checkout_extension("DataInteroperability"),
+    "schematics" = checkout_extension("Schematics")
+  )
+  toolbox.env = new.env(parent = emptyenv())
+  if (missing(tools))
+    tools = list_tools(toolbox)
+  if (toolbox != "*") {
+    tools = gsub("(\\_[a-zA-Z0-9]+)$", "", tools)
+    tools = sprintf("%s_%s", tools, toolbox)
+    names(tools) = gsub("(\\_[a-zA-Z0-9]+)$", "", tools)
+  } else {
+    names(tools) = tools
+  }
+  missing.tools = tools[which(!(tools %in% list_tools(toolbox)))]
+  if (length(missing.tools > 0)) {
+    warning("The following tools could not be found: ", 
+      paste(missing.tools, collapse = ", "), ".", call. = FALSE)
+  tools = tools[!(tools %in% missing.tools)]
+  }
+  for (n in names(tools))
+    assign(n, PythonInR::pyFunction(sprintf("arcpy.%s", tools[[n]])), 
+      pos = toolbox.env)
+  toolbox.env
+}
+
+#' Attach Toolbox
+#'
+#' Attach an ArcGIS toolbox.
+#'
+#' @param envir The environment to attach the toolbox to.
+#' @param name The name of the toolbox.
+#' @param tool.list A list of tools from the toolbox to expose. 
+#'   If NULL, all tools in the toolbox will be exposed.
+#'
+#' @seealso \code{\link{attach_custom}}
+#' @export
+attach_toolbox = function(envir, name, tool.list) {
+  toolbox = import_toolbox(name, tool.list)
+  assign(name, toolbox, pos = envir)
+  invisible(envir)
 }
 
 #' Connect ArcGIS Python
@@ -95,76 +114,87 @@ PythonInR::pyExec(paste('import', module))
 #' connect_ArcPython("C:/Program Files/ArcGIS/Pro/bin/Python/envs/arcgispro-py3",
 #'   Pro = TRUE)
 #' }
+#'
+#' @seealso \code{\link{arcpy_env}}
 #' @export
-connect_ArcPython = function(python_folder, Pro = FALSE, quietly = TRUE){
+connect_ArcPython = function(python_folder, Pro = FALSE, quietly = TRUE) {
   # check if Python is already connected
-  if(PythonInR::pyIsConnected()){
-    if(!quietly)
+  if (PythonInR::pyIsConnected()) {
+    if (!quietly)
       warning("R is already connected Python.")
     return(invisible(NULL))
   }
   # ArcGIS Pro
-  if(Pro){
-    if(missing(python_folder))
+  if (Pro) {
+    if (missing(python_folder))
       python_folder = "C:/Program Files/ArcGIS/Pro/bin/Python/envs/arcgispro-py3"
-    if(!dir.exists(python_folder))
+    if (!dir.exists(python_folder))
       stop("Could not find the ArcGIS Pro Python folder.")
-    if(quietly) 
+    if (quietly)
       suppressPackageStartupMessages(
         PythonInR:::pyConnectWinDll(
-          dllName = "python34.dll", 
-          pyArch = "64bit", 
-          majorVersion = 3, 
-          dllDir = python_folder, 
+          dllName = "python34.dll",
+          pyArch = "64bit",
+          majorVersion = 3,
+          dllDir = python_folder,
           pythonHome = python_folder
         ))
     else
       PythonInR:::pyConnectWinDll(
-        dllName = "python34.dll", 
-        pyArch = "64bit", 
-        majorVersion = 3, 
-        dllDir = python_folder, 
+        dllName = "python34.dll",
+        pyArch = "64bit",
+        majorVersion = 3,
+        dllDir = python_folder,
         pythonHome = python_folder
       )
     return(invisible(NULL))
   }
   # ArGIS Desktop
-  if(missing(python_folder)){
-    if(.Platform$r_arch == "x64"){
-      python_folder = file.path("C:/Python27", 
-        dir("C:/Python27")[grepl("ArcGIS.*x64",dir("C:/Python27"))])
+  if (missing(python_folder)) {
+    if (.Platform$r_arch == "x64") {
+      python_folder = file.path("C:/Python27",
+        dir("C:/Python27")[grepl("ArcGIS.*x64", dir("C:/Python27"))])
       arch = "64bit"
-    } else{
-      python_folder = file.path("C:/Python27", 
-        dir("C:/Python27")[grepl("ArcGIS.*",dir("C:/Python27")) & 
+    } else {
+      python_folder = file.path("C:/Python27",
+        dir("C:/Python27")[grepl("ArcGIS.*", dir("C:/Python27")) &
           !(grepl("ArcGIS.*x64", dir("C:/Python27")))])
       arch = "32bit"
     }
-    if(length(python_folder) > 1)
+    if (length(python_folder) > 1)
       stop("Multiple ArcGIS Desktop Python folders detected. ",
         "Please manually specify the Python path.")
-    if(length(python_folder) < 1)
+    if (length(python_folder) < 1)
       stop("Could not find ArcGIS Desktop Python folder.")
-    if(!file.exists(python_folder))
+    if (!file.exists(python_folder))
       stop("Could not find ArcGIS Desktop Python folder.")
-  }
+    }
   PYTHON_EXE = file.path(python_folder, "python.exe")
-  if(!file.exists(PYTHON_EXE))
+  if (!file.exists(PYTHON_EXE))
     stop("Could not find python.exe in ", python_folder)
-  if(quietly) 
+  if (quietly)
     suppressPackageStartupMessages(PythonInR::pyConnect(PYTHON_EXE))
-  else 
+  else
     PythonInR::pyConnect(PYTHON_EXE)
-  return(invisible(NULL))  
+  PythonInR::pyExec("import arcpy")
+  installinfo = PythonInR::pyGet("arcpy.GetInstallInfo()", 
+    simplify = FALSE)
+  message(sprintf("connected to ArcGIS %s %s (build %s)", 
+    installinfo$ProductName, installinfo$Version, 
+    installinfo$BuildNumber))
+  return(invisible(NULL))
 }
 
 #' ArcPy Environment
 #'
 #' Create an environment that contains ArcPy function interfaces. 
 #'
-#' @param function.list A list of functions from the module to expose. 
-#'   If NULL, all functions in the module will be exposed.
-#' @return An environment object containing arcpy functions.
+#' @param tool.list A list of tools from the toolbox to expose. 
+#'   If NULL, all tools in the toolbox will be exposed.
+#' @param env.list A list of geoprocessing environment settings to 
+#'   expose. If NULL, all environment settings will be exposed.
+#' @return An environment object containing arcpy tools and 
+#'   environment settings.
 #'
 #' @examples
 #' \dontrun{
@@ -175,135 +205,99 @@ connect_ArcPython = function(python_folder, Pro = FALSE, quietly = TRUE){
 #'
 #' arcpy = arcpy_env(list("Delete_management", "Clip_anaylsis"))
 #' ls(arcpy)
+#'
+#' ls(arcpy$env)
+#' arcpy$env$workspace("path_to_workspace")
 #' }
 #'
+#' @seealso \code{\link{connect_ArcPython}}
 #' @export
-arcpy_env = function(function.list = NULL){
-  if(!PythonInR::pyIsConnected())
+arcpy_env = function(tool.list, env.list) {
+  if (!PythonInR::pyIsConnected())
     stop("Python is not connected.")
   # create the arcpy environment
-  arcpyr = import_module("arcpy", function.list)
-  # add environment functions
+  if (missing(tool.list))
+    tool.list = list_tools("*")
+  arcpyr = import_toolbox("*", tool.list)
+  # add arcpy.env settings
   arcpyr.env = new.env(parent = emptyenv())
-  # add worksapce setting functions
-  env.funs = get_funs("arcpy.env", "None")
-  notfuns = get_funs("arcpy.env", "inspect.ismethod")
-  env.funs = env.funs[!(env.funs %in% notfuns)]
-  for(f in env.funs[!grepl("_", env.funs)])
-  assign(f, envfun, pos = arcpyr.env)
+  if (!missing(env.list) && is.null(env.list))
+    return(arcpyr)
+  if (missing(env.list))
+    env.list = list_gpenv()
+  missing.env = env.list[which(!(env.list %in% list_gpenv()))]
+  if (length(missing.env > 0)) {
+    warning("The following environment settings could not be found: ",
+      paste(missing.env, collapse = ", "), ".", call. = FALSE)
+    env.list = env.list[!(env.list %in% missing.env)]
+  }
+  for (f in env.list)
+    assign(f, envfun, pos = arcpyr.env)
   # attach env to arcpy environment
   assign("env", arcpyr.env, pos = arcpyr)
   arcpyr
 }
 
-
-#' Attach Custom Toolbox or Module
+#' Attach a Custom Toolbox
 #'
-#' Attach a custom toolbox or module to ArcPy.
+#' Attach a custom toolbox to ArcPy.
 #'
-#' @param envir The environment to attach the module to.
-#' @param name The name of the toolbox or module to attach.
-#' @param function.list A list of functions from the module to expose. 
-#'   If NULL, all functions in the module will be exposed.
+#' @param envir The environment to attach the toolbox to.
+#' @param name The alias of the toolbox to attach. 
+#'   Underscores \code{_} are not allowed.
+#' @param tool.list A list of tools from the toolbox to expose. 
+#'   If NULL, all tools in the toolbox will be exposed.
 #' @param input.file The input file containing the toolbox.
 #'
+#' @seealso \code{\link{attach_toolbox}}
 #' @export
-attach_custom = function(envir, name, function.list = NULL, input.file){
-  if(!missing(input.file))
-    PythonInR::pyExec(paste0("arcpy.ImportToolbox(", 
-      paste(input.file, name, sep = ","), ")"))
-  attach_module(name, function.list)
+attach_custom = function(envir, name, tool.list, input.file){
+  if (grepl("\\_", name))
+    stop(sprintf("The 'name' argument is valid. Try using '%s' instead.",
+      gsub("\\_", "", name)))
+  if (!file.exists(input.file))
+    stop("The input file could not be found.")
+  PythonInR::pyExec(sprintf('arcpy.ImportToolbox("%s", "%s")', 
+    input.file, name))
+  if (missing(tool.list))
+    tool.list = list_tools(name)
+  attach_toolbox(envir, name, tool.list)
 }
 
-#' Attach Module
+#' List Tools
 #'
-#' Attach a module to arcpy environment.
+#' List ArcGIS tools available in arcpy.
 #'
-#' @param envir The environment to attach the module to.
-#' @param module.name The name fo the module to attach.
-#' @param function.list A list of functions from the module to expose. 
-#'   If NULL, all functions in the module will be exposed.
-#' @param checkout If required, the name of the ArcGIS extension to 
-#'   check out.
-#'
-#' @return (Invisible) The destination environment \code{envir}.
-#'
-#' @details Technically, any Python module can be attached. 
+#' @param toolbox The toolbox name.
+#' @return A list of tools.
 #'
 #' @export
-attach_module = function(envir, module.name, function.list = NULL, 
-  checkout = NA){
-  if(!is.na(checkout))
-    checkout_extension(checkout)
-  module = import_module(module.name, function.list)
-  if(module.name == "arcpy.sa")
-    assign("RasterCalculator", RasterCalculator, module)
-  newname = gsub("(arcpy\\.)", "", module.name)
-  assign(newname, module, pos = envir)
-  invisible(envir)
+list_tools = function(toolbox = "*") { 
+  PythonInR::pyGet(sprintf('arcpy.ListTools("*_%s")', toolbox), 
+    simplify = FALSE)
 }
 
-#' @describeIn attach_module Attach the ArcGIS Spatial Analyst module \code{arcpy.sa}.
+#' List Geoprocessing Environments
+#'
+#' List ArcGIS geoprocessing environment settings, i.e. elements of 
+#'   \code{arcpy.env}.
+#'
+#' @param wc A wild card (regular expression) to filter the results.
+#' @return A list of geoprocessing environment names.
+#'
 #' @export
-attach_sa = function(envir, function.list = NULL)
-  attach_module(envir, "arcpy.sa", function.list, "Spatial")
+list_gpenv = function(wc = "*")
+  PythonInR::pyGet(sprintf('arcpy.ListEnvironments("%s")', wc), 
+    simplify = FALSE)
 
-#' @describeIn attach_module Attach the ArcGIS Geostatistical Analyst module \code{arcpy.ga}.
+#' List Toolboxes
+#'
+#' List ArcGIS toolboxes.
+#'
+#' @return A list of toolboxes.
+#'
 #' @export
-attach_ga = function(envir, function.list = NULL)
-  attach_module(envir, "arcpy.ga", function.list, "Geostats")
-
-#' @describeIn attach_module Attach the ArcGIS Network Analyst module \code{arcpy.na}.
-#' @export
-attach_na = function(envir, function.list = NULL)
-  attach_module(envir, "arcpy.na", function.list, "Network")
-
-#' @describeIn attach_module Attach the ArcGIS 3D Analyst module \code{arcpy.ddd}.
-#' @export
-attach_ddd = function(envir, function.list = NULL)
-  attach_module(envir, "arcpy.ddd", function.list, "3D")
-
-#' @describeIn attach_module ArcGIS Tracking Analyst module \code{arcpy.ta}.
-#' @export
-attach_ta = function(envir, function.list = NULL)
-  attach_module(envir, "arcpy.ddd", function.list, "Tracking")
-
-#' @describeIn attach_module Attach the ArcGIS Business Analyst module \code{arcpy.ba}.
-#' @export
-attach_ba = function(envir, function.list = NULL)
-  attach_module(envir, "arcpy.ba", function.list, "Business")
-
-#' @describeIn attach_module Attach the ArcGIS Data Access module \code{arcpy.da}.
-#' @export
-attach_da = function(envir, function.list = NULL)
-  attach_module(envir, "arcpy.da", function.list)
-
-#' @describeIn attach_module Attach the ArcGIS Pro Mapping module \code{arcpy.mp}. 
-#' @export
-attach_mp = function(envir, function.list = NULL)
-  attach_module(envir, "arcpy.mp", function.list)
-
-#' @describeIn attach_module Attach the ArcGIS Desktop mapping module \code{arcpy.mapping}. 
-#' @export
-attach_mapping = function(envir, function.list = NULL)
-  attach_module(envir, "arcpy.mapping", function.list)
-
-#' @describeIn attach_module Attach the ArcGIS Desktop Fabric module \code{arcpy.fabric}.
-#' @export
-attach_fabric = function(envir, function.list = NULL)
-  attach_module(envir, "arcpy.fabric", function.list)
-
-#' @describeIn attach_module Attach the ArcGIS Desktop Coverage module \code{arcpy.arc}.
-#' @export
-attach_arc = function(envir, function.list = NULL)
-  attach_module(envir, "arcpy.arc", function.list)
-
-#' @describeIn attach_module Attach the ArcGIS Desktop Data Interoperability module \code{arcpy.interop}.
-#' @export
-attach_interop = function(envir, function.list = NULL)
-  attach_module(envir, "arcpy.interop", function.list, "DataInteroperability")
-
-#' @describeIn attach_module Attach the ArcGIS Desktop Schematics module \code{arcpy.schematics}.
-#' @export
-attach_schematics = function(envir, function.list = NULL)
-  attach_module(envir, "arcpy.schematics", function.list, "Schematics")
+list_toolboxes = function() { 
+  tools = list_tools()
+  unique(gsub("^([a-zA-Z0-9]+\\_)", "", tools))
+}
--- README.md
+++ README.md
@@ -1,9 +1,12 @@
-# arcpyrenv
+# arcpyr
 
-An R interface for arcpy. Relies on the virtual Python environment 
-provided by PythonInR to generate function interfaces for arcpy 
-functions and the arcpy environment settings. Functions are accessible
-through creation of an R environment to which arcpy modules and 
-extensions can be attached.
+An R interface for the ArcGIS Python module `arcpy`. Relies on the 
+virtual Python environment provided by `PythonInR` to generate function 
+interfaces for `arcpy` functions and environment settings. 
+`arcpy` modules and extensions are accessible through an R environment 
+object. See the vignette to get started.
 
-In time, I hope ESRI will develop an R library that allows us to interface with ArcGIS in a similar fashion to Python and arcpy, but arcgisbindings doesn't seem to be there yet. Until that happens, arcpyr will have to do.
+In time, I hope ESRI will develop an R library that allows us to 
+interface with ArcGIS in a similar fashion to Python and `arcpy`, but 
+`arcgisbindings` doesn't cut it yet. Until that happens, `arcpyr` will 
+have to do.
